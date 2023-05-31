1cd590d89d8aced5563caa28f99e11741507ce9f
matheusrpd <matheusrpd@gmail.com>
2023-05-30 21:54:52 -0300

    feat: run prebuild and config google autheticate.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,46 @@
-import { ImageBackground, Text, View } from 'react-native'
+import { Alert, ImageBackground, Text, View } from 'react-native'
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+
+import { Button } from '../components/Button'
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
 
 import backgroundImg from '../assets/background.png'
-import { Button } from '../components/Button'
+import { useEffect, useState } from 'react'
+
+WebBrowser.maybeCompleteAuthSession()
 
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  // eslint-disable-next-line no-unused-vars
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  })
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true)
+
+    googleSignIn().then((response) => {
+      if (response.type !== 'success') {
+        setIsAuthenticating(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      if (response.authentication?.idToken) {
+        console.log('TOKEN ', response.authentication.idToken)
+      } else {
+        Alert.alert('Não foi possível conectar-se a sua conta Google.')
+        setIsAuthenticating(false)
+      }
+    }
+  }, [response])
+
   return (
     <View className="flex-1">
       <ImageBackground
@@ -19,7 +56,11 @@ export function SignIn() {
           Gestão de uso de veículos
         </Text>
 
-        <Button title="Entrar com Google" />
+        <Button
+          title="Entrar com Google"
+          isLoading={isAuthenticating}
+          onPress={handleGoogleSignIn}
+        />
       </ImageBackground>
     </View>
   )
